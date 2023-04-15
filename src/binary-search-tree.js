94cd38d8b961c6e5575d0a21cdf50789c986c9e0
Masha Vilenchyk <mariavilenchyk@gmail.com>
2023-04-15 19:31:15 +0300

    feat: add solution tree(April 15, 2023 19.30)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,114 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
+class Node {
+  constructor(value) {
+    this.data = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
 
+  constructor(){
+    this.rootInternal = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootInternal;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootInternal  = addToNodeRec(data, this.rootInternal);
+    
+    function addToNodeRec(data, node){
+
+      if (!node){
+        return new Node(data);
+      }
+
+      if (node.data === data){
+        return node;
+      }
+
+      if (node.data > data) {
+        node.left = addToNodeRec(data, node.left);
+      } else {
+        node.right = addToNodeRec(data, node.right);
+      } 
+      return node
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.find(data) != null
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return findRec(data, this.rootInternal)
+
+    function findRec(data, node){
+      if (!node){
+        return null;
+      }
+
+      if (node.data === data){
+        return node
+      }
+
+      if (node.data > data) {
+        return findRec(data, node.left)
+      } else {
+        return findRec(data, node.right)
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let res = minRec(this.rootInternal)
+
+    if (!res){
+      return null;
+    }
+
+    return res.data
+
+    function minRec(node){
+      if (!node.left){
+        return node
+      }
+      return minRec(node.left)
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let res = maxRec(this.rootInternal)
+
+    if (!res){
+      return null;
+    }
+    
+    return res.data
+
+    function maxRec(node){
+      if (!node.right){
+        return node
+      }
+      return maxRec(node.right)
+    }
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
