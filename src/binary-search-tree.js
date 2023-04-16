8f9545441fbc60828027600dc6b6e66e1d436adf
Masha Vilenchyk <mariavilenchyk@gmail.com>
2023-04-16 11:56:02 +0300

    feat: add solution remove node(April 16, 2023 11.55)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -15,8 +15,7 @@ class Node {
 }
 
 class BinarySearchTree {
-
-  constructor(){
+  constructor() {
     this.rootInternal = null;
   }
 
@@ -25,15 +24,14 @@ class BinarySearchTree {
   }
 
   add(data) {
-    this.rootInternal  = addToNodeRec(data, this.rootInternal);
-    
-    function addToNodeRec(data, node){
+    this.rootInternal = addToNodeRec(data, this.rootInternal);
 
-      if (!node){
+    function addToNodeRec(data, node) {
+      if (!node) {
         return new Node(data);
       }
 
-      if (node.data === data){
+      if (node.data === data) {
         return node;
       }
 
@@ -41,70 +39,101 @@ class BinarySearchTree {
         node.left = addToNodeRec(data, node.left);
       } else {
         node.right = addToNodeRec(data, node.right);
-      } 
-      return node
+      }
+      return node;
     }
   }
 
   has(data) {
-    return this.find(data) != null
+    return this.find(data) != null;
   }
 
   find(data) {
-    return findRec(data, this.rootInternal)
+    return findRec(data, this.rootInternal);
 
-    function findRec(data, node){
-      if (!node){
+    function findRec(data, node) {
+      if (!node) {
         return null;
       }
 
-      if (node.data === data){
-        return node
+      if (node.data === data) {
+        return node;
       }
 
       if (node.data > data) {
-        return findRec(data, node.left)
+        return findRec(data, node.left);
       } else {
-        return findRec(data, node.right)
+        return findRec(data, node.right);
       }
     }
   }
 
   remove(data) {
-
+    this.rootInternal = removeNode(this.rootInternal, data);
+    function removeNode(node, data) {
+      if (!node) {
+        return null;
+      }
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if (node.data < data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      }else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+        if (!node.right) {
+          node = node.right;
+          return node;
+        }
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+        node.data = minFromRight.data;
+        node.right = removeNode(node.right, minFromRight.data)
+        return node;
+      }
+    }
   }
 
   min() {
-    let res = minRec(this.rootInternal)
+    let res = minRec(this.rootInternal);
 
-    if (!res){
+    if (!res) {
       return null;
     }
 
-    return res.data
+    return res.data;
 
-    function minRec(node){
-      if (!node.left){
-        return node
+    function minRec(node) {
+      if (!node.left) {
+        return node;
       }
-      return minRec(node.left)
+      return minRec(node.left);
     }
   }
 
   max() {
-    let res = maxRec(this.rootInternal)
+    let res = maxRec(this.rootInternal);
 
-    if (!res){
+    if (!res) {
       return null;
     }
-    
-    return res.data
 
-    function maxRec(node){
-      if (!node.right){
-        return node
+    return res.data;
+
+    function maxRec(node) {
+      if (!node.right) {
+        return node;
       }
-      return maxRec(node.right)
+      return maxRec(node.right);
     }
   }
 }
